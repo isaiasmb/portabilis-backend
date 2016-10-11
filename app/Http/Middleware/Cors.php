287d928827ae37491b816c0243a020065da66e0d
Buscarino <U6032436@ten.thomsonreuters.com>
2016-10-11 17:50:20 -0300

    Atualizando Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,8 +15,12 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        if ($request->isMethod('options')) {
+            return response('', 200)
+              ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+              ->header('Access-Control-Allow-Headers', 'accept, content-type, 
+                x-xsrf-token, x-csrf-token'); // Add any required headers here
+        }
+        return $next($request);
     }
 }
